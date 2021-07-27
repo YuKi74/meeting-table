9ca869c8e1af190da282486694dfb467904a317b
fairy <1007210564@qq.com>
2021-07-27 20:27:56 +0800

    This is my commit for remove redundant comments. Closes #34

--- backend/business-server/user/models.py
+++ backend/business-server/user/models.py
@@ -10,7 +10,3 @@ class User(AbstractBaseUser):
         'team.Team', on_delete=models.CASCADE, null=True, blank=True)
     USERNAME_FIELD = 'id'
     REQUIRED_FIELDS = ['id', 'name', 'email']
-    # password存储
-
-
-# 写在类的函数和写在序列化器，还有写在view的函数由什么区别
