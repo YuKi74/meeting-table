9907d6f4703ee61ee6e975b01fdd22c524585827
florrie <youyou_2001@126.com>
2021-07-28 21:30:22 +0800

    Team information pop. Closes #7

---
+++ frontend/src/components/team-page/TeaminfoPop.vue
@@ -0,0 +1,242 @@
+<template>
+    <div class="pop">
+        <popover placement="bottomLeft" arrow-point-at-center>
+            <template slot="content">
+                <div id="content">
+                    <div id="title">
+                        <p id="name">{{ name }}</p>
+                        <a-button
+                            id="edit"
+                            icon="edit"
+                            @click="showModal"
+                        ></a-button>
+                        <modal
+                            title="编辑团队信息"
+                            okText="确认"
+                            cancelText="取消"
+                            :visible="visible"
+                            :confirm-loading="confirmLoading"
+                            @ok="handleOk"
+                            @cancel="handleCancel"
+                        >
+                            <div class="create-table">
+                                <form-model
+                                    ref="ruleForm"
+                                    :model="form"
+                                    :rules="rules"
+                                    :label-col="labelCol"
+                                    :wrapper-col="wrapperCol"
+                                    layout="vertical"
+                                >
+                                    <form-model-item
+                                        ref="name"
+                                        label="团队名称"
+                                        prop="name"
+                                    >
+                                        <a-input
+                                            :default-value="form.name"
+                                            v-model="form.name"
+                                            @blur="
+                                                () => {
+                                                    $refs.name.onFieldBlur();
+                                                }
+                                            "
+                                        />
+                                    </form-model-item>
+                                    <form-model-item
+                                        label="团队简介"
+                                        prop="desc"
+                                    >
+                                        <a-input
+                                            v-model="form.desc"
+                                            type="textarea"
+                                            :default-value="form.desc"
+                                            autosize="{minRows:4}"
+                                        />
+                                    </form-model-item>
+                                </form-model>
+                            </div>
+                        </modal>
+                    </div>
+                    <p>{{ description }}</p>
+                    <div id="share-and-dissolve">
+                        <a-button type="danger" @click="success">分享</a-button>
+                        <a-button
+                            id="dissolve"
+                            type="primary"
+                            @click="showDeleteConfirm"
+                            v-if="iscreater"
+                            >解散</a-button
+                        >
+                        <a-button
+                            id="dissolve"
+                            type="primary"
+                            @click="showDeleteConfirm"
+                            v-else
+                            >退出</a-button
+                        >
+                    </div>
+                </div>
+            </template>
+            <a-button id="icon" type="primary" shape="circle">{{
+                name[0]
+            }}</a-button>
+        </popover>
+    </div>
+</template>
+
+<style>
+#icon {
+    font-size: 16px;
+    width: 50px;
+    height: 50px;
+}
+#title {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    width: 250px;
+}
+#edit {
+    flex-shrink: 0;
+}
+#content {
+    display: flex;
+    flex-wrap: wrap;
+    width: 250px;
+    justify-content: center;
+}
+#name {
+    color: var(--black);
+    font-size: 16px;
+    font-weight: 900;
+}
+#share-and-dissolve {
+    display: flex;
+    justify-content: space-around;
+    width: 180px;
+}
+#dissolve {
+    margin-left: 20px;
+}
+</style>
+<script>
+import {
+    Popover,
+    Button,
+    Message,
+    Modal,
+    FormModel,
+    Input,
+} from 'ant-design-vue';
+
+export default {
+    components: {
+        Popover,
+        AButton: Button,
+        Modal,
+        FormModel,
+        FormModelItem: FormModel.Item,
+        AInput: Input,
+    },
+    data: function () {
+        return {
+            name: '干完这单就回家干完这单就回家干完这单就回家干完这单就回家',
+            description:
+                '这是一坨团队介绍这是一坨团队介绍blabla...这是一坨团队介绍这是一坨团队介绍blabla...',
+            // 判断是否为创建者
+            iscreater: true,
+            ModalText: '',
+            visible: false,
+            confirmLoading: false,
+            labelCol: { span: 4 },
+            wrapperCol: { span: 14 },
+            other: '',
+            form: {
+                name: '',
+                desc: '',
+            },
+            rules: {
+                name: [
+                    {
+                        required: true,
+                        message: '请输入团队名称',
+                        trigger: 'blur',
+                    },
+                    {
+                        min: 3,
+                        max: 64,
+                        message: '请控制在3-64个字符以内',
+                        trigger: 'blur',
+                    },
+                ],
+                desc: [
+                    {
+                        required: true,
+                        message: '请输入团队简介',
+                        trigger: 'blur',
+                    },
+                    {
+                        max: 255,
+                        message: '请控制在255个字符以内',
+                        trigger: 'blur',
+                    },
+                ],
+            },
+        };
+    },
+    mounted() {
+        this.form.name = this.name;
+        this.form.desc = this.description;
+    },
+    methods: {
+        success() {
+            var input = document.createElement('input'); // js创建一个input输入框
+            input.value = 'https://antdv.com/components/modal-cn/'; // 将需要复制的文本赋值到创建的input输入框中，this.ruleForm.url这个是我要复制的内容
+            document.body.appendChild(input); // 将输入框暂时创建到实例里面
+            input.select(); // 选中输入框中的内容
+            document.execCommand('Copy'); // 执行复制操作
+            document.body.removeChild(input); // 最后删除实例中临时创建的input输入框，完成复制操作
+            Message.success('分享链接已复制到剪贴板');
+        },
+        showDeleteConfirm() {
+            let isexit = '退出';
+            let dissolve = '解散后不可恢复';
+            if (this.iscreater) {
+                isexit = '解散';
+                dissolve = '解散后不可恢复，且所有会议室文件都会丢失';
+            }
+            Modal.confirm({
+                title: '请确认是否要' + isexit + '团队',
+                content: dissolve,
+                okText: '确定',
+                cancelText: '取消',
+                iconType: 'exclamation-circle',
+                onOk() {
+                    console.log('OK');
+                },
+                onCancel() {
+                    console.log('Cancel');
+                },
+            });
+        },
+        showModal() {
+            this.visible = true;
+        },
+        handleOk(e) {
+            console.log(e);
+            this.ModalText = 'The modal will be closed after two seconds';
+            this.confirmLoading = true;
+            setTimeout(() => {
+                this.visible = false;
+                this.confirmLoading = false;
+            }, 1000);
+        },
+        handleCancel(e) {
+            console.log(e);
+            console.log('Clicked cancel button');
+            this.visible = false;
+        },
+    },
+};
+</script>
