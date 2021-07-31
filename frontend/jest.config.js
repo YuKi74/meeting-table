6b427cc29448a92c5768beb4c14b7a92fcb83f04
paradoxhzy <djsdchzy2936649@163.com>
2021-07-31 12:07:09 +0800

    Modify jest.config.js and install vue-jest and babel-jest. Closes #54

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
     preset: '@vue/cli-plugin-unit-jest',
+    moduleFileExtensions: [
+        'js',
+        'json',
+        // tell Jest to handle `*.vue` files
+        'vue',
+    ],
+    transform: {
+        '.*\\.(vue)$': 'vue-jest',
+        '.*\\.(js)$': 'babel-jest',
+    },
+    transformIgnorePatterns: ['/node_modules/(?!ant-design-vue)'],
 };
