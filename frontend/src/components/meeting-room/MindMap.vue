9e61830a4a0b420a74b64735d6f956d8fab3a536
李安骐 <526774498@qq.com>
2021-08-05 08:14:55 +0000

    Create mindmap component. Ref #76

---
+++ frontend/src/components/meeting-room/MindMap.vue
@@ -0,0 +1,26 @@
+<template>
+    <mindmap v-model="data" height="800"></mindmap>
+</template>
+
+<script>
+import mindmap from '@hellowuxin/mindmap';
+
+export default {
+    components: { mindmap },
+    // props: ['data'],
+    // TODO 从服务器不断获取data
+    data: () => ({
+        data: [
+            {
+                name: '双击进行编辑',
+            },
+        ],
+    }),
+    watch: {
+        data() {
+            // TODO data每次更新上传一次data
+        },
+    },
+};
+</script>
+<style scoped></style>
